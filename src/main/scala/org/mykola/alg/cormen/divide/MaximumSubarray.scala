68c0137c7d3f824dbd20dd27a7b8a43f3258163a
MykolaB <!q1w2e3r4>
2015-07-03 18:11:47 +0300

    added quick sort

---
+++ src/main/scala/org/mykola/alg/cormen/divide/MaximumSubarray.scala
@@ -0,0 +1,8 @@
+package org.mykola.alg.cormen.divide
+
+/**
+ * Created by NBiletskiy on 7/3/2015.
+ */
+class MaximumSubarray {
+
+}
---
+++ src/main/scala/org/mykola/scala/odersky/examples/Sort.scala
@@ -0,0 +1,24 @@
+package org.mykola.scala.odersky.examples
+
+/**
+ * Created by NBiletskiy on 7/3/2015.
+ */
+object QuickSort {
+  def sort(xs: Array[Int]): Array[Int] = {
+    if (xs.length <= 1) xs
+    else {
+      val pivot = xs(xs.length / 2)
+      Array.concat(
+        sort(xs filter (pivot >)),
+            xs filter (pivot ==),
+        sort(xs filter(pivot <))
+      )
+    }
+  }
+}
+
+object Sorter {
+  def main(args: Array[String]) {
+    println(QuickSort.sort(Array(4,7,2,89,0,189)).deep.mkString(","));
+  }
+}
