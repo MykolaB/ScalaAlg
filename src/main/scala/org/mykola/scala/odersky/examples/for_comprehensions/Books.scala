1d2b95be314be9ed87f0f69119f8cbc19334eff7
MykolaB <!q1w2e3r4>
2015-07-10 10:43:35 +0300

    Other

--- src/main/scala/org/mykola/scala/odersky/examples/for_comprehensions/Books.scala
+++ src/main/scala/org/mykola/scala/odersky/examples/for_comprehensions/Books.scala
@@ -3,6 +3,24 @@ package org.mykola.scala.odersky.examples.for_comprehensions
 /**
  * Created by NBiletskiy on 7/9/2015.
  */
-object Books {
 
+case class Book(title: String, authors: List[String])
+
+object Books {
+  val books: List[Book] = List(
+    Book("Structure and Interpretation of Computer Programs",
+      List("Abelson, Harold", "Sussman, Gerald J.")),
+    Book("Principles of Compiler Design",
+      List("Aho, Alfred", "Ullman, Jeffrey")),
+    Book("Programming in Modula2",
+      List("Wirth, Niklaus")),
+    Book("Introduction to Functional Programming",
+    List("Bird, Richard")),
+  Book("The Java Language Specification",
+    List("Gosling, James", "Joy, Bill", "Steele, Guy", "Bracha, Gilad")));
+
+  def main(args: Array[String]) {
+    println(for (b <- books; if (b.title indexOf "Program") > 0)
+      yield b.title)
+  }
 }
